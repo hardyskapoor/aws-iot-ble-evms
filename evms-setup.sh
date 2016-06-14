afc7563cc9c502432adba6d6cdf0bbb670d57869
Abdul-khan <abdul@webequator.com>
2016-06-14 12:24:19 +0100

    wifi script removed

--- evms-setup.sh
+++ evms-setup.sh
@@ -31,11 +31,4 @@ print_status "Running clone for aws-iot-ble-evms"
 cd /opt
 
 exec_cmd 'git clone https://github.com/hardyskapoor/aws-iot-ble-evms.git'
-
-print_status "Preparing /opt/aws-iot-ble-evms/wifi for the WiFi config..."
-mkdir -p /boot/setup/wifi
-touch /boot/setup/wifi/wpa_supplicant.conf
-rm -rf /etc/wpa_supplicant/wpa_supplicant.conf
-exec_cmd 'ln -s /opt/aws-iot-ble-evms/wifi/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf'
-
 print_status "Copy this command and added into crontab \n 2/* * * * * /opt/aws-iot-ble-evms/internet-test.sh "
