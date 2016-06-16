f8914196a35a581617088525e1280a2661f8b46d
Abdul-khan <abdul@webequator.com>
2016-06-16 10:37:22 +0100

    mkdir command commented

--- evms-setup.sh
+++ evms-setup.sh
@@ -38,7 +38,7 @@ print_status "Make the wifi test executeable"
 chmod +x /opt/aws-iot-ble-evms/internet-test.sh
 
 print_status "Setting up cronjob"
-mkdir -p /opt/aws-iot-ble-evms/check-wifi
+# mkdir -p /opt/aws-iot-ble-evms/check-wifi
 exec_cmd 'ln -s /opt/aws-iot-ble-evms/check-wifi /etc/cron.d/check-wifi'
 
 print_status "All done"
