27d18245ebd3b7b4d49865971b6279003cca42a4
Abdul-khan <abdul@webequator.com>
2016-06-16 12:03:55 +0100

    network reboot and sleep line removed

--- internet-test.sh
+++ internet-test.sh
@@ -2,10 +2,7 @@
 x=`ping -c1 google.com 2>&1 | grep unknown`
 if [ !  "$x" =  "" ]; then
 	echo  "Network is down, attempting to restart"
-	# remove add Network reboot
-	# sudo /sbin/ifup --force $wlan
 	sudo reboot
-	sleep 5
 else
 	echo "Network is up and all fine"
 fi
