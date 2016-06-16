a4d0584d86a6f6f3e87ae8e79c526beae989accf
Abdul-khan <abdul@webequator.com>
2016-06-16 11:41:06 +0100

    network reboot replace with device reboot

--- internet-test.sh
+++ internet-test.sh
@@ -2,7 +2,9 @@
 x=`ping -c1 google.com 2>&1 | grep unknown`
 if [ !  "$x" =  "" ]; then
 	echo  "Network is down, attempting to restart"
-	sudo /sbin/ifup --force $wlan
+	# remove add Network reboot
+	# sudo /sbin/ifup --force $wlan
+	sudo reboot
 	sleep 5
 else
 	echo "Network is up and all fine"
