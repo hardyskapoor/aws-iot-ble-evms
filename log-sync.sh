c23d1480606fcb00ea549e204e4542df07324ea6
Abdul-khan <abdul@webequator.com>
2016-08-30 18:28:42 +0100

    log-sync command fixed

--- log-sync.sh
+++ log-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avz  /opt/aws-iot-ble-sensor-log/logs/ root@139.59.173.147:/home/autumn-fair-2016-pi-log/ >> /home/pi/sync.log 2>&1
+rsync -avz  /opt/aws-iot-ble-sensor-log/logs/ root@139.59.173.147:/home/autumn-fair-2016-pi-log/
