03a1b309f84b91758cb2ad5e10e13423c363fbfd
Abdul-khan <abdul@webequator.com>
2016-08-31 11:55:31 +0100

    sync command updated

--- log-sync.sh
+++ log-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avzp --chomod=a+rwx  /opt/aws-iot-ble-sensor-log/logs/ root@139.59.173.147:/home/autumn-fair-2016-pi-log/
+rsync -avz  /opt/aws-iot-ble-sensor-log/logs/ root@139.59.173.147:/home/autumn-fair-2016-pi-log/
