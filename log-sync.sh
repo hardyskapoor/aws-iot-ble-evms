ecc5302780991d1df4d0433078b740f00540d69c
Abdul-khan <abdul@webequator.com>
2016-08-31 16:19:11 +0100

    sync command updated

--- log-sync.sh
+++ log-sync.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-
-rsync -avz  /opt/aws-iot-ble-sensor-log/logs/ root@139.59.173.147:/home/autumn-fair-2016-pi-log/
+#!/usr/bin/expect -f
+spawn rsync -avzp --chmod=a+rwx /opt/aws-iot-ble-sensor-log/logs/ root@139.59.173.147:/home/autumn-fair-2016-pi-log/
+expect "assword:"
+send "45lub82nfpqkc85o90\r"
+interact
