b77b21d1b3fd9d0931481eccf6b39e7e380a6cd0
Abdul-khan <abdul@webequator.com>
2016-09-01 15:45:07 +0100

    ssh link updated

--- log-sync.sh
+++ log-sync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn rsync -avzp --chmod=a+rwx /opt/aws-iot-ble-sensor-log/logs/ root@139.59.173.147:/home/autumn-fair-2016-pi-log/
+spawn rsync -avzp --chmod=a+rwx /opt/aws-iot-ble-sensor-log/logs/ autumnfairlog@www3.g16.pair.com:/usr/home/autumnfairlog/public_html/pilogs/
 expect "assword:"
-send "45lub82nfpqkc85o90\r"
+send "iFNrMcyz\r"
 interact
